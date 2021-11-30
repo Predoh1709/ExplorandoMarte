c38a64a7f82b01d42dd96ff8fc5c71691688b583
Predoh1709 <ph17092003@gmail.com>
2021-11-30 18:15:16 -0300

    Começo do desenvolvimento
    Main já começa a ter interação com o user,  e com a ajuda da classe Campo, já armazena as sondas e evita ter mais de uma em uma posição.

--- src/ExplorandoMarte.java
+++ src/ExplorandoMarte.java
@@ -1,7 +1,21 @@
+
+import exploracao.*; //importando todas minhas classes dentro de exploracao
+import java.util.Scanner; //Scanner vai ajudar na entrada de dados
+
 public class ExplorandoMarte {
 
     public static void main(String[] args) {
+        int X, Y;
 
+        try (Scanner sc = new Scanner(System.in)) { // objeto do tipo Scanner passando o objeto System.in(passa o que é digitado no teclado)
+            System.out.println("Digite o tamanho do campo: ");
+            X = sc.nextInt();
+            Y = sc.nextInt();
+            sc.nextLine(); // le a entrada incluindo o espaço entre as palavras
+
+            // instância do campo com a coordenada
+            Campo campo = new Campo(X, Y);
+        }
     }
-    
+
 }
--- src/exploracao/Campo.java
+++ src/exploracao/Campo.java
@@ -1,5 +1,35 @@
 package exploracao;
 
 public class Campo {
-    
+
+    protected char[][] coordenadas; // protected consigo acessar fora da classe, mas somente no mesmo pacote ou heranca
+    private int x; // private para acessar somente na classe
+    private int y;
+
+    public Campo(int x, int y) {
+        this.x = x + 1;
+        this.y = y + 1;
+        this.coordenadas = new char[this.x + 1][this.y + 1]; // como matriz comeca com 0 
+    }
+
+    public void ocuparEspaco(int i, int j) {
+        coordenadas[i][j] = 'x';
+    }
+
+    public int largura() { // obter a largura
+        return this.x;
+    }
+
+    public int altura() { // obter a altura
+        return this.y;
+    }
+
+    public boolean espacoLivre(int x, int y) { // evitar colisao de sonda e limite do campo
+        boolean x_limite = (x >= 0) && (x < this.x);
+        boolean y_limite = (y >= 0) && (y < this.y);
+        boolean livre = (this.coordenadas[x][y] != 'x');
+
+        return x_limite && y_limite && livre;
+    }
+
 }
